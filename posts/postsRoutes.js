40f3b75c3009a55e65e37fd5c9ad7fdf58f180ed
devin-bielejec <devin.bieleje@gmail.com>
2019-09-17 17:50:36 -0400

    get post by id

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -55,6 +55,19 @@ router.get("/", (req, res) => {
     })
 })
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    console.log('aoweifj')
+    db.findById(id)
+    .then(post => {
+        console.log(post);
+        post.length >= 1 ? res.status(200).json(post) : res.status(404).json({message: "The post with the specified ID does not exist."})
+    })
+    .catch(err => {
+        res.status(500).json({error: "The post information could not be retrieved."})
+    })
+})
+
 module.exports = router;
 
 
